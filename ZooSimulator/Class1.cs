29d6519d1efec6516e8a9a0c5315ab19cbd5c0f4
yoonjisu1201 <yoonjisu1201@gmail.com>
2026-03-04 18:21:52 +0900

    실습 중간제출합니다.

---
+++ ZooSimulator/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Animal
+{
+    protected string _sound;
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Animal(string sound, string name, int age)
+    {
+        _sound = sound;
+        Name = name;
+        Age = age;
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine($"{Name}이(가) 먹이를 먹습니다.");
+    }
+
+    public void MakeSound()
+    {
+        Console.WriteLine($"{Name}: {_sound}");
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"이름: {Name}, 나이: {Age}살");
+    }
+}
+
+
+class Lion : Animal
+{
+    public Lion(string sound, string name, int age) : base(sound, name, age)
+    {
+    }
+
+    public void Hunt() => Console.WriteLine($"{Name}이(가) 사냥을 합니다.");
+}
+
+class Elephant : Animal
+{
+
+    public void SprayWater() => Console.WriteLine($"{Name}이(가) 물을 뿌립니다.");
+}
+
+class Penguin : Animal
+{
+
+    public void Swim() => Console.WriteLine($"{Name}이(가) 수영을 합니다.");
+}
